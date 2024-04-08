17c97bf0b0918f89c4820250389e7132b3a3ac8c
IrynaSlavinska <133566139+IrynaSlavinska@users.noreply.github.com>
2024-04-08 17:25:43 +0300

    routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
+// import { lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Layout from './Layout/Layout';
+// const HomePage = lazy(() => import('pages/HomePage/HomePage'));
+// const LoginPage = lazy(() => import('pages/LoginPage/LoginPage'));
+// const ContactsPage = lazy(() => import('pages/ContactsPage/ContactsPage'));
+// const RegisterPage = lazy(() => import('pages/RegisterPage/RegisterPage'));
+// const NotFound = lazy(() => import('pages/NotFound/NotFound'));
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<div>Home</div>} />
+        <Route path="/about" element={<div>About</div>} />
+        <Route path="/projects" element={<div>Projects</div>} />
+
+        {/* <Route path="*" element={<NotFound to={'/'} />}></Route> */}
+      </Route>
+    </Routes>
   );
 };
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,31 @@
+import { Outlet, NavLink } from 'react-router-dom';
+import { Suspense } from 'react';
+
+const Layout = () => {
+  return (
+    <div>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/projects">Projects</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <Suspense fallback={null}>
+          <Outlet />
+        </Suspense>
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
