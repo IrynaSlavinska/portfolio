6c5cb5c5b94f856d1f5f3cb193cef60ebb50966e
IrynaSlavinska <133566139+IrynaSlavinska@users.noreply.github.com>
2024-09-20 18:09:31 +0000

    Deploying to gh-pages from @ IrynaSlavinska/portfolio@e73f049ced936edd2ac801ee3fd82011fb62db5f 🚀

---
+++ static/js/895.d857bf7a.chunk.js
@@ -0,0 +1,33 @@
+"use strict";(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[895],{875:(e,i,t)=>{t.r(i),t.d(i,{default:()=>j});const o=t.p+"static/media/web-studio.ff8e6f2e428b63bf5649.JPG",a=t.p+"static/media/simply-chocolate.bfeeb197311b0aa518d4.JPG",r=t.p+"static/media/veggieboost.b1c50b35976969b275b4.JPG",s=t.p+"static/media/your-energy.c6d36b6893838c5da06c.JPG",p=t.p+"static/media/movies-gallery.5ae7950333fe7d45688f.JPG",n=t.p+"static/media/empty-picture.5545b4ba38f78ed821a0.JPG",l=[{id:1,projectName:"WEB Studio",github:"https://github.com/IrynaSlavinska/goit-markup-hw-06",livepage:"https://irynaslavinska.github.io/goit-markup-hw-06/",technologies:["HTML","CSS","Java Script"],role:"Developer",type:"Individual project",part:"Whole project",imageSRC:o,description:"Landing page for web development studio with responsive design for different screens."},{id:2,projectName:"Simply Chocolate",github:"https://github.com/IrynaSlavinska/simply_chocolate",livepage:"https://irynaslavinska.github.io/simply_chocolate/",technologies:["HTML","CSS","Java Script"],role:"Developer",type:"Individual project",part:"Whole project",imageSRC:a,description:"Landing page for chocolate workshop with responsive design for different screens."},{id:3,projectName:"Veggieboost",github:"https://github.com/IrynaSlavinska/project-OurDreamTeam",livepage:"https://irynaslavinska.github.io/project-OurDreamTeam/",technologies:["HTML","CSS","Java Script","Vite"],role:"Team Lead, Developer",type:"Team project",part:"3 easy steps...",imageSRC:r,description:"Website with an adaptive layout for shop with organic products."},{id:4,projectName:"Your Energy",github:"https://github.com/konstantin-it-lysenko/projectDreamTeamJS",livepage:"https://konstantin-it-lysenko.github.io/projectDreamTeamJS/",technologies:["HTML","CSS","Java Script","Vite"],libraries:["axios","Notiflix","BasicLightbox","Lodash"],role:"Developer",type:"Team project",part:"Hero section, Devs modal",imageSRC:s,description:"'Your Energy' is a web application dedicated to promoting physical well-being by offering affordable and effective workout solutions. The platform is designed to provide accessible and user-firendly fitness resources to help individuals maintain a healthy and active lifestyle."},{id:5,projectName:"Movies Gallery",github:"https://github.com/IrynaSlavinska/goit-react-hw-05-movies",livepage:"https://irynaslavinska.github.io/goit-react-hw-05-movies/",technologies:["React.js","Styled components"],libraries:["axios","react-router-dom","emotion/styled","react-icons","react-loader-spinner"],role:"Developer",type:"Individual project",part:"Whole project",imageSRC:p,description:""},{id:6,projectName:"Phonebook",github:"https://github.com/IrynaSlavinska/goit-react-hw-08-phonebook",livepage:"https://irynaslavinska.github.io/goit-react-hw-08-phonebook/",technologies:["React.js","Styled components","Redux Toolkit"],libraries:["axios","react-router-dom","emotion/styled","react-icons","react-loader-spinner","notiflix","reduxjs/toolkit","redux-persist"],role:"Developer",type:"Individual project",part:"Whole project",imageSRC:n,description:"An app was developed to provide users with a convenient platform for saving contacts. In project was provided an authorization for security and getting exactly yours data."},{id:7,projectName:"Phonebook",github:"",livepage:"",technologies:[],role:"Developer",type:"Individual project",part:"Whole project",imageSRC:n,description:""},{id:8,projectName:"Task PRO",github:"https://github.com/lizaklimova/task-pro",livepage:"https://lizaklimova.github.io/task-pro/",technologies:[],role:"Scrum master, Developer",type:"Team project",part:"Create/update board modal",imageSRC:t.p+"static/media/task-pro.b9877e646023a3c2921b.JPG",description:"TaskPro is an application designed for efficient task and project management. It provides a convenient way to organize tasks and collaborate with teams."}];var c=t(441),d=t(659),h=t(218);const g=h.A.ul`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 30px;
+  padding: 20px;
+`,m=h.A.li`
+  width: calc((100% - 60px) / 3);
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  border: 1px solid var(--green);
+  border-radius: 8px;
+  padding: 16px;
+  box-shadow: 1px 2px 5px 0px var(--green);
+`,v=h.A.div`
+  border-radius: 4px;
+  width: 100%;
+  height: 276px;
+  overflow: hidden;
+  box-shadow: 1px 2px 5px 0px var(--green);
+`,u=h.A.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`,b=h.A.h2`
+  text-align: center;
+  color: var(--green);
+`,x=h.A.a`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+`;var y=t(579);const f=()=>(0,y.jsx)(g,{children:l.map((e=>{let{id:i,projectName:t,type:o,role:a,imageSRC:r,github:s,livepage:p,description:n}=e;return(0,y.jsxs)(m,{children:[(0,y.jsx)(v,{children:(0,y.jsx)(u,{src:r,alt:t})}),(0,y.jsx)(b,{children:t}),(0,y.jsxs)(x,{href:s,target:"_blank",rel:"noopener noreferrer",children:[(0,y.jsx)(c.Vx.Provider,{value:{color:"#8ce436",size:"20px"},children:(0,y.jsx)(d.ioy,{})}),"GitHub Repository"]}),(0,y.jsxs)(x,{href:p,target:"_blank",rel:"noopener noreferrer",children:[(0,y.jsx)(c.Vx.Provider,{value:{color:"#8ce436",size:"20px"},children:(0,y.jsx)(d.$vZ,{})})," ","Live Page"]}),(0,y.jsxs)("p",{children:["Type: ",o]}),(0,y.jsxs)("p",{children:["Role: ",a]}),(0,y.jsx)("p",{children:"Technologies: "}),(0,y.jsxs)("p",{children:["Description: ",n]})]},i)}))}),j=()=>(0,y.jsx)("div",{children:(0,y.jsx)(f,{})})}}]);
+//# sourceMappingURL=895.d857bf7a.chunk.js.map
